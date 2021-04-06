d5c98fba0014b894823f789a9ef185ceb836dc7c
2lol555 <2lol555@minmov.sk>
2021-04-06 15:41:53 +0200

    Made the cookie consent better

--- js/gdpr.js
+++ js/gdpr.js
@@ -19,14 +19,19 @@ const saveToStorage = () => storageType.setItem(consentPropertyName, true);
 window.onload = () => {
     const consentPopup = document.getElementById("consent-popup");
     const acceptBtn = document.getElementById("suhlas-button");
+    const declineBtn = document.getElementById("nesuhlas-button");
 
     const acceptFn = event => {
         saveToStorage(storageType);
         consentPopup.classList.add("hidden");
         dataLayer.push({'event': 'accepted'});
     }
+    const declineFn = event => {
+        consentPopup.classList.add("hidden");
+    }
 
     acceptBtn.addEventListener("click", acceptFn);
+    declineBtn.addEventListener("click", declineFn);
 
     if(shouldShowConsent()) {
         setTimeout(() => {
